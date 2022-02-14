a66054561c962c83659c951821c2b46586906652
Colombe05 <41596639+Colombe05@users.noreply.github.com>
2022-02-14 15:58:14 +0100

    Update diag.py

--- diag.py
+++ diag.py
@@ -69,6 +69,6 @@ class DiagResource(object):
             #resp.body = 'Treatment=' + "error: party=" + req.params["party"]
         
 # falcon.API instances are callable WSGI apps
-app = falcon.API()
+app = falcon.app()
 
 app.add_route( , DiagResource())
